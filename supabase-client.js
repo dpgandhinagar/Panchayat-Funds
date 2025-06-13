e536c54e122cd18bb26a799afcce67b87d7a6ef5
dpgandhinagar <dpgandhinagar1@gmail.com>
2025-06-13 18:46:31 +0530

    Add files via upload

---
+++ supabase-client.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.0/+esm'
+
+const supabaseUrl = 'https://jyailcsclxjzcnyrnsic.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5YWlsY3NjbHhqemNueXJuc2ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4MjM1OTksImV4cCI6MjA2MTM5OTU5OX0.TVh104V8SnfeRTWywzYU96j-7uKtbzX6Tf8kjQbPwKk'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
